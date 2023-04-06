cf8b10657a5b155bb309575dd198115717519198
CONebula <127456034+CONebula@users.noreply.github.com>
2023-04-06 12:04:04 -0600

    So I have moved some items and renamed a few files
    -New Files names
    -New Elements
    -New Style
    -Not yet finished.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// I will console log Thanks for visiting my Personal Website. This schould fulfill the requirement. /*
+
+
+const strAlpha= "Thanks for viewing my personal website!";
+console.log(`${Alpha}. Hope you enjoyed.`);
+
+
+
+
+
